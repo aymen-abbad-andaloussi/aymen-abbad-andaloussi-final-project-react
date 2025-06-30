c7b7e7699b1d0d0502d4264ee0bfdc642407145a
aymen abbad andaloussi <aymenandaloussi69@gmail.com>
2025-06-30 19:38:35 +0100

    edit images

--- src/assets/imagesFile/Images.jsx
+++ src/assets/imagesFile/Images.jsx
@@ -1,14 +1,14 @@
-import logoNav from "../images/logo.png";
-import carousel1 from "../images/carousel1.jpg";
-import carousel2 from "../images/carousel2.jpg";
-import carousel3 from "../images/carousel3.jpg";
-import shopy1 from '../images/shopy1.jpg'
-import shopy2 from '../images/shopy2.jpg'
-import shopy3 from '../images/shopy3.jpg'
-import shopy4 from '../images/shopy4.jpg'
-import shopy5 from '../images/shopy5.jpg'
-import shopy6 from '../images/shopy6.jpg'
-import shopy7 from '../images/shopy7.jpg'
-import shopy8 from '../images/shopy8.jpg'
+import logoNav from "../../assets/images/logo.png";
+import carousel1 from "../../assets/images/carousel1.jpg";
+import carousel2 from "../../assets/images/carousel2.jpg";
+import carousel3 from "../../assets/images/carousel3.jpg";
+import shopy1 from '../../assets/images/shopy1.jpg'
+import shopy2 from '../../assets/images/shopy2.jpg'
+import shopy3 from '../../assets/images/shopy3.jpg'
+import shopy4 from '../../assets/images/shopy4.jpg'
+import shopy5 from '../../assets/images/shopy5.jpg'
+import shopy6 from '../../assets/images/shopy6.jpg'
+import shopy7 from '../../assets/images/shopy7.jpg'
+import shopy8 from '../../assets/images/shopy8.jpg'
 
 export {logoNav, carousel1, carousel2, carousel3, shopy1, shopy2, shopy3, shopy4, shopy5, shopy6, shopy7, shopy8 }
