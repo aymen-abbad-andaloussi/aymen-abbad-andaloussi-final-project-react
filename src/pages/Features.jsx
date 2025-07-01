90c6ba28682821a4627186fa460708c2ddced014
aymen abbad andaloussi <aymenandaloussi69@gmail.com>
2025-07-01 17:16:53 +0100

    finish features

--- src/pages/Features.jsx
+++ src/pages/Features.jsx
@@ -1,7 +1,16 @@
+import Footer from "../Components/Footer";
+import Nav from "../Components/Nav";
+
 export default function Features() {
   return (
-    <div>
-      
+    <div className="h-[100vh] flex flex-col justify-between">
+      <Nav />
+
+      <div className="mt-[10vh] w-full h-[25vh] bg-black flex justify-center items-center">
+        <h1 className="text-5xl text-white font-bold">FEATURES</h1>
+      </div>
+
+      <Footer />
     </div>
-  )
+  );
 }
