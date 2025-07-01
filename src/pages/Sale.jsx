dbaeed501d29764443c7a012f4b73f8bb3cab0d1
aymen abbad andaloussi <aymenandaloussi69@gmail.com>
2025-07-01 17:08:00 +0100

    finish sale

--- src/pages/Sale.jsx
+++ src/pages/Sale.jsx
@@ -1,7 +1,21 @@
+import { FaAngleRight } from "react-icons/fa";
+import Footer from "../Components/Footer";
+import Nav from "../Components/Nav";
+import { Link } from "react-router-dom";
+
 export default function Sale() {
   return (
-    <div>
-      
+    <div className="flex flex-col justify-between h-[100vh] ">
+      <Nav />
+        <div className="">
+                  <div className="mt-[13vh] flex text-black/60 text-sm items-center w-fit pl-12 gap-1">
+          <Link className="hover:text-red-700 transition" to="/">Home</Link>
+          <FaAngleRight />
+          <Link className="hover:text-red-700 transition" to="/Sale">Search</Link>
+        </div>
+        <h1 className="pl-100 text-4xl pt-10 ">Search</h1>
+        </div>
+      <Footer />
     </div>
   )
 }
